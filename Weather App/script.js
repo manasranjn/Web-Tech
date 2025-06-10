770f50a9172e5f8649dbccb51856d6340bf3e457
Manas Ranjan Bera <manasbera484@gmail.com>
2025-06-10 08:09:07 +0530

    react init

--- Weather App/script.js
+++ Weather App/script.js
@@ -16,7 +16,6 @@ async function checkWeather(city) {
         var data = await response.json();
         console.log(data);
 
-
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
