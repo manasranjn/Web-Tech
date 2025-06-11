ca0e427fccc7544bd6ef7833dcf0e115f40aecf1
Manas Ranjan Bera <manasbera484@gmail.com>
2025-06-11 14:18:41 +0530

    REACT CLASS 3

--- ReactJS/cra-app/src/App.js
+++ ReactJS/cra-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import StudentDetails from "./Components/StudentDetails";
+import Navbar from "./Components/Navbar";
+import ClassComponent from "./Components/ClassComponent";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>App</h1>
+      <h1>First Component</h1>
+      <StudentDetails /> */}
+      <Navbar />
+      <ClassComponent />
     </div>
-  );
+  )
 }
 
 export default App;
