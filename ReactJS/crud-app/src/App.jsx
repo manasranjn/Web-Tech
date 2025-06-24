4664022950a074590d73bd642e1d4f1865108541
Manas Ranjan Bera <manasbera484@gmail.com>
2025-06-24 10:01:53 +0530

    Crud-app

---
+++ ReactJS/crud-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-green-400 p-10">
+      <h1 className="text-9xl text-orange-600 font-bold">App</h1>
+    </div>
+  );
+};
+
+export default App;
